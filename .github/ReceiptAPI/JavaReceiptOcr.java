695c3c93a0ed84e9446e620fc84a44895bdb2a9f
Adriana Shuaipaj <92822481+ashua2@users.noreply.github.com>
2022-10-07 16:04:26 -0500

    API Implementation

---
+++ .github/ReceiptAPI/JavaReceiptOcr.java
@@ -0,0 +1,35 @@
+// this code given by asprise in java
+// needed dependencies are org.apache.httpcomponents:httpclient:4.5.13 and org.apache.httpcomponents:httpmime:4.5.13
+
+import java.io.File;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+ 
+public class JavaReceiptOcr {
+	public static void main(String[] args) throws Exception {
+		String receiptOcrEndpoint = "https://ocr.asprise.com/api/v1/receipt"; // Receipt OCR API endpoint
+		File imageFile = new File("receipt.jpg");
+
+		System.out.println("=== Java Receipt OCR Demo - Need help? Email support@asprise.com ===");
+
+		try (CloseableHttpClient client = HttpClients.createDefault()) {
+			HttpPost post = new HttpPost(receiptOcrEndpoint);
+			post.setEntity(MultipartEntityBuilder.create()
+				.addTextBody("client_id", "TEST")       // Use 'TEST' for testing purpose
+				.addTextBody("recognizer", "auto")      // can be 'US', 'CA', 'JP', 'SG' or 'auto'
+				.addTextBody("ref_no", "ocr_java_123'") // optional caller provided ref code
+				.addPart("file", new FileBody(imageFile))    // the image file
+				.build());
+
+			try (CloseableHttpResponse response = client.execute(post)) {
+				System.out.println(EntityUtils.toString(response.getEntity())); // Receipt OCR result in JSON
+			}
+		}
+	}
+}
+
