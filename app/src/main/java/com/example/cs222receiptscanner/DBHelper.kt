882afc8d6595ffc79ffb9280f06699607379b6e9
Jaden Lee <joonhee4@illinois.edu>
2022-12-06 20:21:45 -0600

    finished its lit

--- app/src/main/java/com/example/cs222receiptscanner/DBHelper.kt
+++ app/src/main/java/com/example/cs222receiptscanner/DBHelper.kt
@@ -9,22 +9,16 @@ import android.database.sqlite.SQLiteOpenHelper
 class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
     SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
 
-    // below is the method for creating a database by a sqlite query
     override fun onCreate(db: SQLiteDatabase) {
-        // below is a sqlite query, where column names
-        // along with their data types is given
         val query = ("CREATE TABLE " + TABLE_NAME + " ("
                 + ID_COL + " INTEGER PRIMARY KEY, " +
                 ITEM_COL + " TEXT," +
                 PRICE_COL + " TEXT" + ")")
 
-        // we are calling sqlite
-        // method for executing our query
         db.execSQL(query)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
-        // this method is to check if table already exists
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
         onCreate(db)
     }
@@ -39,66 +33,40 @@ class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
         db.execSQL(query)
         db.execSQL(createq)
     }
-    // This method is for adding data in our database
+
     fun addName(name : String, age : String ){
 
-        // below we are creating
-        // a content values variable
         val values = ContentValues()
 
-        // we are inserting our values
-        // in the form of key-value pair
         values.put(ITEM_COL, name)
         values.put(PRICE_COL, age)
 
-        // here we are creating a
-        // writable variable of
-        // our database as we want to
-        // insert value in our database
         val db = this.writableDatabase
 
-        // all values are inserted into database
         db.insert(TABLE_NAME, null, values)
 
-        // at last we are
-        // closing our database
         db.close()
     }
 
-    // below method is to get
-    // all data from our database
     fun getName(): Cursor? {
-
-        // here we are creating a readable
-        // variable of our database
-        // as we want to read value from it
         val db = this.readableDatabase
 
-        // below code returns a cursor to
-        // read data from the database
         return db.rawQuery("SELECT * FROM " + TABLE_NAME, null)
 
     }
 
     companion object{
-        // here we have defined variables for our database
 
-        // below is variable for database name
         private val DATABASE_NAME = "CS222_Receipt Scanner"
 
-        // below is the variable for database version
         private val DATABASE_VERSION = 1
 
-        // below is the variable for table name
         val TABLE_NAME = "Items"
 
-        // below is the variable for id column
         val ID_COL = "id"
 
-        // below is the variable for name column
         val ITEM_COL = "name"
 
-        // below is the variable for age column
         val PRICE_COL = "price"
     }
 }
--- app/src/main/java/com/example/cs222receiptscanner/gsonToJson.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cs222receiptscanner
-
-import java.io.File
-
-public class gsonToJson(apikey: String, rec: String, f: File) {
-    private val api_key = apikey
-    private val recognizer = rec
-    private val file = f
-}
